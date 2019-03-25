b95407aff8b18d3c8cdda6c481bd55702bbcfe91
hyq1124 <48701070+hyq1124@users.noreply.github.com>
2019-03-25 13:34:50 +0800

    Update 飞船.py

--- 游戏/飞船.py
+++ 游戏/飞船.py
@@ -1,5 +1,7 @@
 import pygame
 from random import randint
+
+
 class Game():
     # 初始化
     pygame.init()
@@ -32,23 +34,23 @@ class AirShip():
 
     # 向上移动
     def move_up(self):
-        if airship.y > 0:
-            airship.y -= 0.1
+        if self.y > 0:
+            self.y -= 1
 
     # 向下移动
     def move_down(self):
         if self.y < 315:
-            self.y += 0.1
+            self.y += 1
 
     # 向左移动
     def move_left(self):
         if self.x > 0:
-            self.x -= 0.1
+            self.x -= 1
 
     # 向右移动
     def move_right(self):
         if self.x < 570:
-            self.x += 0.1
+            self.x += 1
 
     @staticmethod
     def move():
@@ -67,28 +69,71 @@ class AirShip():
             airship.move_right()
 
 
-class Bown():
+# 动画精灵
+# 用该方法承载障碍物使其移动
+class SkierClass(pygame.sprite.Sprite):
     def __init__(self):
-        self.x = 700
-        self.y = randint(0, 350)
+        pygame.sprite.Sprite.__init__(self)
+        # 加载图片
+        self.image = pygame.image.load("start.jpg")
+        # 矩形框选图片获取位置，大小
+        self.rect = self.image.get_rect()
+        # 设置位置
+        self.rect.left, self.rect.top = [Game.background.get_width()+randint(50, 200), randint(0, Game.background.get_height() - 50 )]
+        # 设置速度
+        self.speed = [-1, 0]
 
-    def createbown(self):
-        pygame.draw.circle(Game.background, [randint(0, 256), randint(0, 256), randint(0, 256)], [self.x, self.y], 15)
+    def move(self):
+        self.rect = self.rect.move(self.speed)
+
+
+
+# 创建图形
+# class Bown():
+#     def __init__(self):
+#         self.x = 700
+#         self.y = randint(0, 350)
+#
+#     def createbown(self):
+#         pygame.draw.circle(Game.background, [randint(0, 256), randint(0, 256), randint(0, 256)], [self.x, self.y], 15)
 
     # circle(Surface, color, pos, radius, width=0)
+
 
 if __name__ == '__main__':
     # 创建对象
     g = Game()
     airship = AirShip()
-
+    skl = []
+    for i in range(5):
+        sk = SkierClass()
+        skl.append(sk)
+    sc = 0
     while True:
         # 添加背景图
         g.window.blit(g.background, (0, 0))
         # 添加飞船图片
         g.window.blit(g.airship, (airship.x, airship.y))
+        # 添加障碍物图片
         # 移动飞船
         airship.move()
+        # 移动障碍物
+        for j in range(5):
+            g.window.blit(skl[j].image, skl[j].rect)
+            skl[j].move()
+
+        # 障碍物循环出现
+        for k in range(5):
+            if skl[k].rect.left < -randint(50, 200):
+                skl[k].rect.left, skl[k].rect.top = [Game.background.get_width() + randint(50, 1000), randint(0, Game.background.get_height() - 50) ]
+
+        # 调整障碍物速度
+        sc += 1
+
+        if sc >= 500:
+            for l in range(5):
+                skl[l].speed[0] -= 1
+            sc = 0
 
         # 创建事件
         walk = pygame.event.get()
@@ -100,23 +145,8 @@ if __name__ == '__main__':
                 exit()
             # 判断是否按键
             # if l.type == pygame.KEYDOWN:
-                # if l.key == pygame.K_DOWN or l.key == pygame.K_s:
-                #     if airship.y < 315:
-                #         airship.y += 5
-                #         print('↓')
-                # if l.key == pygame.K_UP or l.key == pygame.K_w:
-                #     if airship.y > 0:
-                #         airship.y -= 5
-                #         print('↑')
-                # if l.key == pygame.K_LEFT or l.key == pygame.K_a:
-                #     if airship.x > 0:
-                #         airship.x -= 5
-                #         print('←')
-                # if l.key == pygame.K_RIGHT or l.key == pygame.K_d:
-                #     if airship.x < 570:
-                #         airship.x += 5
-                #         print('→')
-
+            # if l.key == pygame.K_DOWN or l.key == pygame.K_s:
+            #    pass
         # 停止音乐
         # pygame.mixer.music.stop()
 
@@ -124,4 +154,4 @@ if __name__ == '__main__':
         pygame.display.update()
         # pygame.display.flip()
         # 延时
-        # pygame.time.delay()
+        pygame.time.delay(10)
